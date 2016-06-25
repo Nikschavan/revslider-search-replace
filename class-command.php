6ba3e709caf4e7018167a67f0807f3c9de11692c
nikhilc@bsf.io <nikhilc@bsf.io>
2016-06-25 21:36:23 +0530

    added doc for the command

--- class-command.php
+++ class-command.php
@@ -1,6 +1,33 @@
 <?php
 
 class Revslider_Search_Replace extends WP_CLI_Command {
+	/**
+	 * Clean up unused, orphaned and duplicated data in your WordPress
+	 *
+	 * ## OPTIONS
+	 *
+	 * <id>
+	 *      ID of the slider, also takes "all" as option where it will search accross all the sliders
+	 *
+	 * <source-url>
+	 *      Source URL
+	 *
+	 * <destination-url>
+	 *      destination URL
+	 *
+	 * [--network]
+	 *      Search Replace the strings in Revolution sliders throughout all the sites in multisite network
+	 *
+	 * ## EXAMPLES
+	 *
+	 *  1. wp rsr 2 <source-url> <destination-url>
+	 *      - This will search replace the strings in the slider with is "2"
+	 *  2. wp rsr all <source-url> <destination-url>
+	 *      - This will search replace the strings on all the sliders on the site
+	 *  3. wp rsr all <source-url> <destination-url> --network
+	 *		- This command will will search replace the strings on all sliders accross all the sites in multisite network.
+	 *
+	*/
 
 	public $slider;
 
