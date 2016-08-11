7e1a00cdd992f11453b466c2ed796a11d5f1674f
nikhilc@bsf.io <nikhilc@bsf.io>
2016-08-11 10:32:32 +0530

    version bump to v0.3

--- revslider-search-replace.php
+++ revslider-search-replace.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Revolution Slider Search Replace CLI
-Version: 0.2
+Version: 0.3
 Description: WP CLI Command to Search Replace Strings in revolution slider
 Author: Nikhil Chavan
 Author URI: http://www.nikhilchavan.com
